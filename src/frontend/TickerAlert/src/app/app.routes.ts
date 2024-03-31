38e4ec6946edf1e0e6a25800f848c081cce3d91a
Exequiel <exe.pine@gmail.com>
2024-03-31 11:53:25 -0300

    feature: frontend project adding angular material

--- src/frontend/TickerAlert/src/app/app.routes.ts
+++ src/frontend/TickerAlert/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { AlertsComponent } from './features/alerts/alerts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: AlertsComponent }];
