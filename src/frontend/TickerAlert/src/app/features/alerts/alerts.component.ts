da1c48fa1bb1c5147fab8e271165728375e17085
Exequiel <exe.pine@gmail.com>
2024-04-02 18:15:00 -0300

    feature: adding first request to test communication with endpoint

--- src/frontend/TickerAlert/src/app/features/alerts/alerts.component.ts
+++ src/frontend/TickerAlert/src/app/features/alerts/alerts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Alert } from './models/alert.model';
 import { MatTableModule } from '@angular/material/table';
+import { AlertsService } from './services/alerts.service';
 
 @Component({
   selector: 'app-alerts',
@@ -8,6 +9,7 @@ import { MatTableModule } from '@angular/material/table';
   imports: [MatTableModule],
   templateUrl: './alerts.component.html',
   styleUrl: './alerts.component.css',
+  providers: [AlertsService],
 })
 export class AlertsComponent implements OnInit {
   public displayedColumns: string[] = [
@@ -19,22 +21,28 @@ export class AlertsComponent implements OnInit {
   ];
   public alerts: Alert[] = [];
 
+  constructor(private alertsService: AlertsService) {}
+
   ngOnInit(): void {
-    this.alerts = [
-      {
-        tickerName: 'BTC',
-        targetPrice: 70000,
-        actualPrice: 60000,
-        difference: 10000,
-        state: 'PENDING',
-      },
-      {
-        tickerName: 'ETH',
-        targetPrice: 3800,
-        actualPrice: 3650,
-        difference: 150,
-        state: 'PENDING',
-      },
-    ];
+    this.alertsService.getAlerts().subscribe((result) => {
+      this.alerts = result;
+    });
+
+    // this.alerts = [
+    //   {
+    //     tickerName: 'BTC',
+    //     targetPrice: 70000,
+    //     actualPrice: 60000,
+    //     difference: 10000,
+    //     state: 'PENDING',
+    //   },
+    //   {
+    //     tickerName: 'ETH',
+    //     targetPrice: 3800,
+    //     actualPrice: 3650,
+    //     difference: 150,
+    //     state: 'PENDING',
+    //   },
+    // ];
   }
 }
