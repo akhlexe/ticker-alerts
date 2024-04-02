3f9378da5fad02e5d5f930bdde6a6160d917b06c
Exequiel <exe.pine@gmail.com>
2024-04-02 18:32:05 -0300

    refactor: old angular SSR app deleted

--- src/frontend/TickerAlert/src/app/app.routes.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { AlertsComponent } from './features/alerts/alerts.component';
-
-export const routes: Routes = [{ path: '', component: AlertsComponent }];
--- src/frontend/TickerAlert/src/app/features/alerts/models/alert.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Alert {
-  tickerName: string;
-  targetPrice: number;
-  actualPrice: number;
-  difference: number;
-  state: string;
-}
