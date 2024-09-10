2f0bc82bc3cdc2aa19764ac658134bb74dec33ba
Exequiel <exe.pine@gmail.com>
2024-09-09 22:34:52 -0300

    feat: adding icons in the financial-asset-component

--- src/frontend/TickerAlertApp/src/app/features/financial-assets/financial-assets.component.ts
+++ src/frontend/TickerAlertApp/src/app/features/financial-assets/financial-assets.component.ts
@@ -5,11 +5,19 @@ import { Observable } from 'rxjs';
 import { CompanyProfileDto } from '../../shared/services/financial-asset/models/financial-asset.model';
 import { FinancialAssetsService } from '../../shared/services/financial-asset/financial-assets.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+const MatModules = [
+  MatProgressSpinnerModule,
+  MatIcon,
+  MatButtonModule
+]
 
 @Component({
   selector: 'app-financial-assets',
   standalone: true,
-  imports: [CommonModule, MatProgressSpinnerModule],
+  imports: [CommonModule, MatModules],
   templateUrl: './financial-assets.component.html',
   styleUrl: './financial-assets.component.css'
 })
