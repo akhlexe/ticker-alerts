8cfe71d3187553954021298e7e5d898ecc1cd417
Exequiel <exe.pine@gmail.com>
2024-09-09 21:25:42 -0300

    feat: adding spinner to financial asset component

--- src/frontend/TickerAlertApp/src/app/features/financial-assets/financial-assets.component.ts
+++ src/frontend/TickerAlertApp/src/app/features/financial-assets/financial-assets.component.ts
@@ -4,26 +4,24 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { CompanyProfileDto } from '../../shared/services/financial-asset/models/financial-asset.model';
 import { FinancialAssetsService } from '../../shared/services/financial-asset/financial-assets.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-financial-assets',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatProgressSpinnerModule],
   templateUrl: './financial-assets.component.html',
   styleUrl: './financial-assets.component.css'
 })
 export class FinancialAssetsComponent implements OnInit {
   companyProfile$: Observable<CompanyProfileDto> | undefined;
-  private selectedAssetId = '';
 
   constructor(private route: ActivatedRoute, private financialAssetsService: FinancialAssetsService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       const financialAssetId = params.get('id') || '';
-      this.selectedAssetId = financialAssetId;
       this.companyProfile$ = this.financialAssetsService.getFinancialAssetProfile(financialAssetId);
-      console.log('Asset ID:', this.selectedAssetId);
     })
   }
 }
