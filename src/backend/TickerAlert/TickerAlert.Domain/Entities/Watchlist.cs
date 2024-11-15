40a0d2eaed24ec12b24206419c6d642c8c9e9308
Exequiel <exe.pine@gmail.com>
2024-11-15 10:44:36 -0300

    feature: adding watchlist entities

---
+++ src/backend/TickerAlert/TickerAlert.Domain/Entities/Watchlist.cs
@@ -0,0 +1,20 @@
+﻿using TickerAlert.Domain.Common;
+
+namespace TickerAlert.Domain.Entities;
+
+public class Watchlist : Entity
+{
+    public const string DEFAULT_WATCHLIST_NAME = "main";
+
+    public Guid UserId { get; private set; }
+    public string Name { get; private set; }
+
+    public static Watchlist Create(Guid id, Guid userId, string name = DEFAULT_WATCHLIST_NAME) => new(id, userId, name);
+
+    private Watchlist(Guid id, Guid userId, string name)
+            : base(id)
+    {
+        UserId = userId;
+        Name = name;
+    }
+}
---
+++ src/backend/TickerAlert/TickerAlert.Domain/Entities/WatchlistItem.cs
@@ -0,0 +1,15 @@
+﻿using TickerAlert.Domain.Common;
+
+namespace TickerAlert.Domain.Entities;
+
+public class WatchlistItem : Entity
+{
+    public Guid FinancialAssetId { get; private set; }
+
+    public static WatchlistItem Create(Guid id, Guid financialAssetId) => new(id, financialAssetId);
+
+    private WatchlistItem(Guid id, Guid financialAssetId) : base(id)
+    {
+        FinancialAssetId = id;
+    }
+}
