60d6ce5dad29534ae74af5c2aaf5af4f07d917e6
Exequiel <exe.pine@gmail.com>
2024-11-15 10:49:17 -0300

    feature: adding entity framework configurations for watchlist

---
+++ src/backend/TickerAlert/TickerAlert.Infrastructure/Persistence/Configurations/WatchlistItemConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TickerAlert.Domain.Entities;
+
+namespace TickerAlert.Infrastructure.Persistence.Configurations;
+
+internal sealed class WatchlistItemConfiguration : IEntityTypeConfiguration<WatchlistItem>
+{
+    public void Configure(EntityTypeBuilder<WatchlistItem> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.FinancialAssetId)
+            .IsRequired();
+    }
+}
