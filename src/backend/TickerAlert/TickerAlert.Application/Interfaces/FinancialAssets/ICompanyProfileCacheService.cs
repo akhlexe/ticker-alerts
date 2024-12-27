43c220888a1f4cd3b534dcd5cf50a58abb80e14c
Exequiel <exe.pine@gmail.com>
2024-12-26 21:17:52 -0300

    feat: adding company profile cache service

---
+++ src/backend/TickerAlert/TickerAlert.Application/Interfaces/FinancialAssets/ICompanyProfileCacheService.cs
@@ -0,0 +1,9 @@
+﻿using TickerAlert.Application.Services.StockMarket.Dtos;
+
+namespace TickerAlert.Application.Interfaces.FinancialAssets;
+
+public interface ICompanyProfileCacheService
+{
+    Task<CompanyProfileDto?> GetCompanyProfileDto(Guid financialAssetId);
+    Task SaveCompanyProfileDto(Guid financialAssetId, CompanyProfileDto companyProfileDto);
+}
---
+++ src/backend/TickerAlert/TickerAlert.Application/Services/FinancialAssets/CompanyProfileCacheService.cs
@@ -0,0 +1,17 @@
+﻿using TickerAlert.Application.Common.Cache;
+using TickerAlert.Application.Interfaces.FinancialAssets;
+using TickerAlert.Application.Services.StockMarket.Dtos;
+
+namespace TickerAlert.Application.Services.FinancialAssets;
+
+internal class CompanyProfileCacheService(ICacheService cacheService) : ICompanyProfileCacheService
+{
+    private const string NamespacePrefix = "CompanyProfile";
+    private const int TTL = 3600;
+
+    public async Task<CompanyProfileDto?> GetCompanyProfileDto(Guid financialAssetId) 
+        => await cacheService.GetAsync<CompanyProfileDto>(NamespacePrefix, financialAssetId.ToString());
+
+    public async Task SaveCompanyProfileDto(Guid financialAssetId, CompanyProfileDto companyProfileDto)
+        => await cacheService.SetAsync(NamespacePrefix, financialAssetId.ToString(), companyProfileDto, TimeSpan.FromMinutes(TTL));
+}
